e16a0392a180033946862aa08a0bf01fd17ff3e3
James Collings <james@jclabs.co.uk>
2014-10-27 22:08:26 +0000

    switch widget with existing view

--- templates/content-event-widget.php
+++ templates/content-event-widget.php
@@ -1,21 +1,4 @@
-<?php //do_action( 'jce/before_event_content' ); ?>
+<?php 
 
-<article class="jce-event">
-
-	<header class="jce-event-header">
-		WIDGET
-		<?php //do_action( 'jce/event_header' ); ?>
-	</header>
-
-	<div class="jce-event-content">
-		<?php //do_action('jce/event_content'); ?>
-		<?php //the_excerpt(); ?>
-	</div>
-
-	<footer class="jce-event-footer">
-		<?php //do_action('jce/event_footer'); ?>
-	</footer>
-
-</article>
-
-<?php //do_action( 'jce/after_event_content' ); ?>
+include 'content-event.php';
+?>
